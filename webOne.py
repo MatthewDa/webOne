59a9ff0e850e534f134e568bac45f2129559ad4c
adrisj7 <adrisj7@gmail.com>
2015-07-20 13:14:58 -0400

    Variables without Updated Reader

--- webOne.py
+++ webOne.py
@@ -15,15 +15,31 @@ def homepage():
 def senGenPage():
     return render_template("senGenPage.html",height = 600, title = 'Team Serpent - Sentence Generator',sentence = sengen())
 
+
+
+
 @derp.route("/markov/",methods = ['POST','GET'])
 def markovPage():
+    book_list = ["grimm","junglebook","oz","tom","wonderland"]
+#length = len(book_list) #FOR HTML PURPOSES
+    name = ["Grimm's Fairy Tales","The Jungle Book","The Wonderful Wizard of Oz","The Adventures of Tom","Alice's adventures in Wonderland"]
+    checked = []
+    for i in range(len(book_list)):
+        checked += "0"
+
     if request.method == 'GET':
         return render_template("markovPage.html",height = 400, title = 'Team Serpent - Markov Chains')
     if request.method == 'POST':
         results = request.form
         newList = results.getlist("books")
+
+        for i in range(len(newList)):
+            for j in range(len(book_list)):
+                if book_list[j] == newList[i]:
+                    checked[j] == "1"
+
         if newList == []:
-            return render_template("markovPage.html",text = "ERROR[7]:\n No checkboxes checked",height = 400, title = 'Team Serpent - Markov Chains')
+            return render_template("markovPage.html",text = "ERROR[7]:\n No checkboxes checked",height = 400, title = 'Team Serpent - Markov Chains',checked = checked,book_list = book_list,name = name)
         q = open('data/bookList','w')
         q.write('')
         q.close()
@@ -32,22 +48,36 @@ def markovPage():
             q.write(i + '\n')
         q.close()
         writeB()
-        return render_template("markovPage.html",text = create("data/","allBooks"),height = 400, title = 'Team Serpent - Markov Chains')
-        
+        return render_template("markovPage.html",text = create("data/","allBooks"),height = 400, title = 'Team Serpent - Markov Chains',checked = checked,book_list = book_list,name = name)
+
+
+cipher_d_t = ""
+cipher_d_l1 = ""#results["letter1"]
+cipher_d_l2 = ""#results["letter2"]
 
 @derp.route("/ceasarcipher/", methods=["GET","POST"])
 def cipherPage():
+
     if request.method=="POST":
         results=request.form
         if results["mode"]=="e":
             modeText="Encrypted"
-            resultText=ceasarCipher(results["letter1"], results["letter2"], results["message"])
-        if results["mode"]=="d":
+        elif results["mode"]=="d":
             modeText="Decrypted"
-            resultText=ceasarDecipher(results["letter1"], results["letter2"], results["message"])
-        return render_template("cipherPage.html",height = 400, title = 'Team Serpent - Ceaser Cipher', mode=modeText, result=resultText)
+        resultText=ceasarDecipher(results["letter1"], results["letter2"], results["message"])
+        cipher_d_l1 = results["letter1"]
+        cipher_d_l2 = results["letter2"]
+        cipher_d_t = results["message"]    
+        
+        return render_template("cipherPage.html",height = 400, title = 'Team Serpent - Ceaser Cipher', mode=modeText, result=resultText,l1 = cipher_d_l1,l2 = cipher_d_l2,text = cipher_d_t)
     elif request.method=="GET":
-        return render_template("cipherPage.html",height = 400, title = 'Team Serpent - Ceaser Cipher', mode="Encrypted/Decrypted", result="")
+        return render_template("cipherPage.html",height = 400, title = 'Team Serpent - Ceaser Cipher', mode="Encrypted/Decrypted", result="",l1 = "",l2 = "",text = "")
+
+
+
+
+
+
 
 if __name__=="__main__":
     derp.debug=True
